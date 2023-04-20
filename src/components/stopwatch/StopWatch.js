bfb8f5d8906ed6dc274719d1aae90f73d66f3938
harsh-sharma-z <harsh.ramakant.sharma@gmail.com>
2023-04-20 11:34:00 +0530

    commit

--- src/components/stopwatch/StopWatch.js
+++ src/components/stopwatch/StopWatch.js
@@ -1,24 +1,33 @@
 import React from "react";
-import { useState } from "react";
+import { useRef,useState } from "react";
 const StopWatch = () => {
   const [seconds, setSeconds] = useState(0);
-
+  const refValue = useRef(null);
   const startTimer = () => {
-    setInterval(() => {
+    clearInterval(refValue.current);
+    refValue.current=setInterval(() => {
       setSeconds((prevSeconds) => prevSeconds + 0.01);
     }, 10);
   };
 
   const stopTimer = () => {
+    clearInterval(refValue.current);
+  };
+
+  const resetTimer = () => {
     setSeconds(0);
   };
 
   return (
-    <div>
+    <div style={{textAlign:"center"}}>
       <p  style={{width:"30%"}}>Timer: {seconds.toFixed(2)}</p>
 
-      <button onClick={startTimer}>Start</button>
-      <button onClick={stopTimer}>Stop</button>
+      <span>
+      <button style={{margin:"10px 20px " , display:"inline"}} onClick={startTimer}>Start</button>
+      <button style={{margin:"10px 20px " , display:"inline"}} onClick={stopTimer}>Stop</button>
+      <button style={{margin:"10px 20px " , display:"inline"}} onClick={resetTimer}>Reset</button>
+      </span>
+      
     </div>
   );
 };
